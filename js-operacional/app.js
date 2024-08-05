d87e9b2717d1b0ed83202e035bc413111e233fe8
lucasromr <lucasdanromero@gmail.com>
2024-08-05 18:26:42 -0400

    pasos

---
+++ js-operacional/app.js
@@ -0,0 +1,30 @@
+/*
+    DOMContentLoaded sirve para controlar
+    que primero se cargue TODO el documento
+    HTML, por ultimo javascript
+*/
+document.addEventListener("DOMContentLoaded", function(){
+    // esta es zona segura
+    //console.log(document.getElementById("txtNombre").value);
+    //console.log(document.getElementById("txtApellido").value);
+});
+
+// añadiendo evento clic al botón mostrar
+const btnMostrar = document.getElementById("btnMostrar");
+btnMostrar.addEventListener("click", function() {
+    //console.log('haciendo click');
+    //alert("hice clic");
+    //recuperando info del formulario
+    const nombres = document.getElementById("txtNombre").value;
+    const apellido = document.getElementById("txtApellido").value;
+    const cedula = document.getElementById("txtCedula").value;
+    const dirección = document.getElementById("txtDirección").value;
+    console.log('nombres ' +nombres+ ', apellidos ' +apellido+ ', cedula ' + cedula+ ', dirección ' + dirección);
+    alert('nombres ' +nombres+ ', apellidos ' +apellido+ ', cedula ' + cedula+ ', dirección ' + dirección);
+
+});
+
+function hola(nombre, apellido) {
+    console.log(nombre, apellido);
+    // codigo reutilizable
+}
